34b7a3713ced17c2b852fb2bfacb005406ff2a81
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-11-17 20:10:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,5 +25,5 @@ end
 
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["sports cars", "flatiron school"]
-    all_my_favs = using_concat(my_favorite_things, more_favs)
+    all_my_favs.concat(my_favorite_things, more_favs)
   end

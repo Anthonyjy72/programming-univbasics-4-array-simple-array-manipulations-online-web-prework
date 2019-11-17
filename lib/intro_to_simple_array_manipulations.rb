c6e6a8ab187f8a71ea90281fb27bd981a20b7d80
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-11-17 20:11:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,3 +27,6 @@ my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["sports cars", "flatiron school"]
     all_my_favs.concat(my_favorite_things, more_favs)
   end
+  
+list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  
